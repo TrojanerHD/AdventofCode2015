495285dfab450bb1547cde28b41511ee6624a0c6
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-10-12 03:14:49 +0200

    Added solution to day 1

---
+++ src/01/index.ts
@@ -0,0 +1,30 @@
+import Day from '../day.ts';
+import { Response } from '../main.ts';
+
+export default class Day01 implements Day {
+  main(data: string): Response {
+    const parAsNumbers: number[] = data
+      .split('')
+      .map((element: string): -1 | 1 => (element === '(' ? 1 : -1));
+    let found: boolean = false;
+    return [
+      {
+        message: 'The instructions take Santa to floor',
+        value: parAsNumbers.reduce((a: number, b: number): number => a + b),
+      },
+      {
+        message:
+          'The position of the character that causes Santa to first enter the basement',
+        value: parAsNumbers.reduce(
+          (a: number, b: number, i: number): number => {
+            if (a === -1) {
+              found = true;
+              return i;
+            }
+            return found === true ? a : a + b;
+          }
+        ),
+      },
+    ];
+  }
+}
