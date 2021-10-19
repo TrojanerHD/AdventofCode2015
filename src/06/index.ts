1b691e9ee67290e7ebd2172e43d69be96f1834e2
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-10-20 00:27:22 +0200

    Attempted to clean up day 6's code

--- src/06/index.ts
+++ src/06/index.ts
@@ -1,6 +1,11 @@
 import Day from '../day.ts';
 import { Response } from '../main.ts';
 
+interface Instructions {
+  action: string;
+  firstCoordinates: number[];
+  secondCoordinates: number[];
+}
 export default class Day06 implements Day {
   main(data: string): Response {
     const instructionSet: string[] = data.split(/\r?\n/);
@@ -11,36 +16,29 @@ export default class Day06 implements Day {
       lightBrightness.push(Array(1000).fill(0));
     }
     for (const instruction of instructionSet) {
-      const firstSplit: string[] = instruction.split(' through ');
-      const action: string = firstSplit[0].split(/ \d/)[0];
-      const firstCoordinateString: string = firstSplit[0].split(/ (?=\d)/)[1];
-      const firstCoordinates: number[] = firstCoordinateString
-        .split(',')
-        .map((value: string): number => Number(value));
-      const secondCoordinateString: string = firstSplit[1];
-      const secondCoordinates: number[] = secondCoordinateString
-        .split(',')
-        .map((value: string): number => Number(value));
+      const { action, firstCoordinates, secondCoordinates }: Instructions =
+        this.splitInstructions(instruction);
+
       for (let y: number = firstCoordinates[1]; y <= secondCoordinates[1]; y++)
         for (
           let x: number = firstCoordinates[0];
           x <= secondCoordinates[0];
           x++
         ) {
-          lights[y][x] =
-            action === 'turn on'
-              ? true
-              : action === 'turn off'
-              ? false
-              : !lights[y][x];
-          lightBrightness[y][x] +=
-            action === 'turn on'
-              ? 1
-              : action === 'turn off' && lightBrightness[y][x] > 0
-              ? -1
-              : action === 'toggle'
-              ? 2
-              : 0;
+          switch (action) {
+            case 'turn on':
+              lights[y][x] = true;
+              lightBrightness[y][x]++;
+              break;
+            case 'turn off':
+              lights[y][x] = false;
+              if (lightBrightness[y][x] > 0) lightBrightness[y][x]--;
+              break;
+            case 'toggle':
+              lights[y][x] = !lights[y][x];
+              lightBrightness[y][x] += 2;
+              break;
+          }
         }
     }
     return [
@@ -62,5 +60,19 @@ export default class Day06 implements Day {
           .reduce((a: number, b: number): number => a + b),
       },
     ];
+  }
+
+  private splitInstructions(instruction: string): Instructions {
+    const firstSplit: string[] = instruction.split(' through ');
+    const action: string = firstSplit[0].split(/ \d/)[0];
+    const firstCoordinateString: string = firstSplit[0].split(/ (?=\d)/)[1];
+    const firstCoordinates: number[] = firstCoordinateString
+      .split(',')
+      .map((value: string): number => Number(value));
+    const secondCoordinateString: string = firstSplit[1];
+    const secondCoordinates: number[] = secondCoordinateString
+      .split(',')
+      .map((value: string): number => Number(value));
+    return { action, firstCoordinates, secondCoordinates };
   }
 }
